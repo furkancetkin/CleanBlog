6248f1ffaf53a7ea4c41700692d32773d2f49a4d
Furkan Cetkin <furkan.cetkin@hotmail.com>
2022-07-10 22:46:33 +0300

    v5

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const Post = require('./models/Post');
 const path = require('path');
+const methodOverride = require('method-override');
+const postControllers = require('./controllers/postControllers');
+const pageControllers = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -18,30 +21,21 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index', {
-    posts,
-  });
-});
-app.get('/posts/:id', async (req, res) => {
-  console.log(req.params.id)
-  const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+app.get('/', postControllers.getAllPosts);
+app.get('/posts/:id', postControllers.getPost);
+app.post('/posts', postControllers.createPost);
+app.put('/posts/:id', postControllers.updatePost);
+app.delete('/posts/:id', postControllers.deletePost);
+
+app.get('/about', pageControllers.getAboutPage);
+app.get('/add_post', pageControllers.getAddPage);
+app.get('/posts/edit/:id', pageControllers.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
